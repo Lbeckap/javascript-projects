73ee463bc2c84cc47f5237df822cc191e3666f67
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-06-18 07:36:22 -0400

    data-variables-conditionals studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,4 +1,25 @@
 // Initialize Variables below
+let date = 'Monday 2019-03-18';
+let time = '10:05:34 AM';
+let astronautCount = 7;
+let astronautStatus = 'ready';
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = '100%';
+let weatherStatus = 'clear';
+let preparedForLiftOff = true;
+let launchSequenceMsg = 'All systems are go! Initiating space shuttle launch sequence.\n';
+let launchDetailMsg = `Date: ${date}\n Time: ${time}\n Astronaut Count: ${astronautCount}\n Crew Mass: ${crewMassKg} kg\n Fuel Mass: ${fuelMassKg} kg\n Shuttle Mass: ${shuttleMassKg} kg\n Total Mass: ${totalMassKg} kg\n Fuel Temperature: ${fuelTempCelsius} C\n Weather Status: ${weatherStatus}\n`;
+let byeMsg = 'Have a safe trip astronauts!';
+let dashes = '--------------------------------------\n';
+let terminateMsg = 'Required conditions NOT met! Launch operations terminated.\n';
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
@@ -13,3 +34,15 @@
 // add logic below to verify the weather status is clear
 
 // Verify shuttle launch can proceed based on above conditions
+if (
+    astronautCount <= 7 &&
+    astronautStatus === 'ready' &&
+    totalMassKg < maximumMassLimit &&
+    (fuelTempCelsius >= -300 || fuelTempCelsius <= -150) &&
+    fuelLevel === '100%' &&
+    weatherStatus === 'clear'
+  ) {
+    console.log(launchSequenceMsg, dashes, launchDetailMsg, dashes, byeMsg);
+  } else {
+    console.log(terminateMsg, dashes, launchDetailMsg);
+  }
