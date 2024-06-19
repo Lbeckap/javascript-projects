50eaa496c6652b2a02444ca4d7a29bbf01e71c0c
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-06-19 16:06:58 -0400

    Exercises: String

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,25 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+console.log(String(num).length);
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
+num = 123.45;
+let numString = String(num);
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+let stringLength = numString.length;
+
+//evaluates as truthy if there is a '.' at any index, but zero
+console.log('numString: ' + numString)
+console.log(numString.includes('.'));
+
+if (numString.includes('.')) {
+    
+    console.log(stringLength - 1);
+    
+} else {
+    console.log(stringLength);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -3,15 +3,27 @@
 let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+let sliceJ = language.slice(0, 1);
+let sliceS = language.slice(4, 5);
+console.log(`${sliceJ}${sliceS}`);
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+let indexJ = language.indexOf('J');
+let indexS = language.indexOf('S');
+console.log(language[indexJ] + language[indexS]);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+console.log(`The abbreviation for '${language}' is '${language[0]}${language[4]}'.`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+console.log(language.toUpperCase().slice(0, 4).indexOf('V'));
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+
+let TitleCase = notTitleCase.replace('t', 'T').replace('c', 'C');
+console.log(TitleCase);
+
