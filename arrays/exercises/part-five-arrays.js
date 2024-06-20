2c580255114ff8d86022e804a1339b8659ae10e3
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-06-20 14:51:36 -0400

    arrays exercises 1-6

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,21 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
+let strArr = str.split('e');
+console.log(strArr);
+console.log('The purpose of the parameter inside of () is to act as an delimeter');
+console.log('Original String:',str);
 
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+let joinArr = arr.join(' ');
+console.log(joinArr);
+console.log('The purpose of the parameter inside out () is to denote if the new array will be separated by commas or a specified separator string');
+console.log('Original Array:', arr);
 
 //3) Do split or join change the original string/array?
+console.log('The original string or array is not changed.');
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+let cargoHoldArr = cargoHold.split(',').sort().join(',');
+console.log(cargoHoldArr);
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -4,7 +4,18 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+let concatArr = holdCabinet1.concat(holdCabinet2);
+console.log(concatArr, 'The original array is not altered.');
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+let slice1 = holdCabinet1.slice(1, 3);
+let slice2 = holdCabinet2.slice(3, 5);
+console.log(slice1, slice2, '\nThe original array is not altered.');
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+holdCabinet2.sort();
+console.log('This difference is that verses an array in place and returns the reference to the same array, the first array element now becoming the last, and the last array element becoming the first. \n sort(); sorts the elements of an array in place and returns the reference to the same array, now sorted. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.');
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+console.log('Yes, both original arrays are altered. toSorted(); can be used to sort without mutating the original array');
