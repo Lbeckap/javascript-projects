33ca07758c2a9fd7a2cbc9b406657d547e7cde06
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-06-24 16:51:01 -0400

    Functions Exercises

---
+++ functions/functions.js
@@ -0,0 +1,81 @@
+
+
+console.log(makeLine(5));
+
+function makeLine(size, char = '#') {
+    let line = '';
+    for (let i = 0; i < size; i++) {
+        line += char;
+    }
+    return line;
+}
+
+console.log(makeSquare(5));
+
+function makeSquare(size) {
+    let square = '';
+    for (let i = 0; i < size; i++) {
+        square += `\n${makeLine(size)}`; 
+    } 
+    return square;
+}
+
+console.log(makeRectangle(5, 3));
+
+function makeRectangle(width, height) {
+    let rectangle = '';
+    for (let i = 0; i < height; i++) {
+        rectangle += `\n${makeLine(width)}`;
+    }
+    return rectangle;
+}
+
+console.log(makeDownwardStairs(5));
+
+function makeDownwardStairs(height) {
+    let triangle = '';
+    for (let i = 0; i < height; i++) {
+        triangle += `\n${makeLine(i + 1)}`;
+    }
+    return triangle;
+}
+
+console.log(makeSpaceLine(3, 5));
+
+function makeSpaceLine(numSpaces, numChars) {
+    let spaceLine = ''
+    function insertSpaces(spaceCount) {
+        for (let i = 0; i < spaceCount; i++) {
+            spaceLine += ' ';
+        }
+        return spaceLine;
+    }
+    insertSpaces(numSpaces);
+    spaceLine += (makeLine(numChars));
+    insertSpaces(numSpaces);
+
+    return spaceLine;
+}
+
+console.log(makeIsoscelesTriangle(5));
+
+function makeIsoscelesTriangle(height) {
+    let triangle = '';
+    for (let i = 0; i < height; i++){
+        triangle += `\n${(makeSpaceLine(height - i - 1, 2*i + 1))}`;
+    }
+    return triangle;
+}
+
+console.log(makeDiamond(5));
+
+function makeDiamond(height) {
+   let diamond = '';
+    for (let i = 0; i < height; i++){
+        diamond += `\n${(makeSpaceLine(height - i - 1, 2*i + 1))}`;
+    }
+    diamond += `\n${diamond.split('').reverse().join('')}`;
+
+    return diamond;
+}
+
